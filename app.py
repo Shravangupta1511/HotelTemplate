85c2a06ba9529dd9bcc26c7a08a4748a296d6c29
Shravangupta1511 <shravangupta1511@gmail.com>
2026-01-30 14:48:26 +0530

    website ready without any issues

--- app.py
+++ app.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/amenities.html')
+def amenities():
+    return render_template('amenities.html')
+
+@app.route('/booking.html')
+def booking():
+    return render_template('booking.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/room.html')
+def room():
+    return render_template('room.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
